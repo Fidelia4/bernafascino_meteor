e36c5f2bdc9c0a28eb4380699023a1b21b2b12d8
Fidelia <fideliagbd@gmail.com>
2025-11-13 17:48:13 +0100

    correction

--- imports/ui/Footer.jsx
+++ imports/ui/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
       {/* Contact */}
       <div className="text-sm">
-        <h3 className="font-bold mb-2 text-center">Contact</h3>
+        <h3 className="font-bold mb-2 text-center -ml-38">Contact</h3>
         <p>Tél: +229 01 54 10 34 65 / </p>
          <p> +229 01 96 33 31 09</p> 
         <p>Email: fideliagbd@gmail.com</p>
@@ -19,7 +19,7 @@ const Footer = () => {
 
       {/* Réseaux sociaux */}
       <div className="text-sm">
-        <h3 className="font-bold mb-2 text-center">Suivez-nous</h3>
+        <h3 className="font-bold mb-2 -ml-12 text-center">Suivez-nous</h3>
         <div className="flex gap-4 mt-2">
           <a href="https://www.instagram.com/s/aGlnaGxpZ2h0OjE3ODk1MTE5MTAwMjgxNzgz?story_media_id=3705078725777849586_64561994540&igsh=MXNwdmh4MXF3NDFtNQ==" target="_blank" rel="noopener noreferrer">
             <img src="images/instafid.png" alt="instagram" className="w-8 h-8" />
@@ -34,7 +34,7 @@ const Footer = () => {
 
       {/* Mentions légales */}
       <div className="text-sm">
-        <h3 className="font-bold mb-2 text-center">Mentions légales</h3>
+        <h3 className="font-bold mb-2 text-center -ml-12">Mentions légales</h3>
         <ul>
           <li><a href="#conditions" className="underline">Politique de confidentialité</a></li>
           <li><a href="#conditions" className="underline">Conditions générales</a></li>
